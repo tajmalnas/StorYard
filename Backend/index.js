2de10f8da7e831d2ef9c28f4b6d8a898bee4e9f4
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2024-02-08 10:15:18 +0530

    added mvc architecture to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/user.route');
 
 const app = express();
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(
-    () => {
+mongoose.connect(process.env.MONGO).then(() => {
         console.log('Connected to database');
-    }
-    ).catch(
-        () => {
-            console.log('Connection failed');
-        }
-    );
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+    }).catch(() => {
+        console.log('Connection failed');
     }
 );
+
+
+app.use('/api/user', userRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
